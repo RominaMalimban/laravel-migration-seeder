62ae90554371203a3c5e32e7434c5d44a6b2d603
RominaMalimban <romina.lyn@hotmail.it>
2023-02-01 16:09:29 +0100

    creazione migrations

---
+++ database/migrations/2023_02_01_143914_create_travel_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->date('departure_date');
+            $table->date('arrival_date');
+            $table->integer('price') -> unsigned();
+            $table->text('description');
+            $table->integer('duration') ->unsigned();
+            $table->text('itinerary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+};
